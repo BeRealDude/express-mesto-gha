17ee054222f808a5532f764317d82b736886f8b5
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-03-14 23:39:45 +0300

    feat: controllers, routers users

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routerUser = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.get('/', (req, res) => {
   res.send('hello');
 });
+
+app.use('/users', routerUser);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
