7a9ef9eb32a80547c60b15da4fc6c39ca1e43311
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-05-26 18:03:49 +0300

    feat: logger

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const express = require('express');
 const mongoose = require('mongoose');
+const PageNotFound = require('./error/page-not-found');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errorCenter } = require('./middlewares/error-center');
+
 const routerSignup = require('./routes/signup');
 const routerSignin = require('./routes/signin');
 
 const routerUser = require('./routes/users');
 const routerCard = require('./routes/cards');
-const { errorCenter } = require('./middlewares/error-center');
-
-const PageNotFound = require('./error/page-not-found');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,16 +20,18 @@ const app = express();
 app.use(express.json());
 
 const limiter = rateLimit({
-  max: 10000, // Перед сдачей исправить на 200!!!
+  max: 1000, // Перед сдачей исправить на 200!!!
   windowMs: 60 * 60 * 1000,
   message: 'Слишком много запросов с этого IP-адреса',
 });
 
-app.use(limiter);
-
 app.use(helmet());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(requestLogger);
+
+app.use(limiter);
 
 app.use('/', routerSignup);
 app.use('/', routerSignin);
@@ -38,9 +42,10 @@ app.use('/cards', routerCard);
 app.use((req, res, next) => {
   next(new PageNotFound('Страница не найдена'));
 });
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use(errorCenter);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
